396236d0d3ee458dc200365e7830f5a356876d2a
Esteban Gallego <egallego00@gmail.com>
2024-09-20 09:03:53 -0400

    Pagination test

--- tests/Feature/Controllers/CommentController/DeleteTest.php
+++ tests/Feature/Controllers/CommentController/DeleteTest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Tests\Feature\Controllers\PostController;
+namespace Tests\Feature\Controllers\CommentController;
 
 use App\Models\Comment;
 use Tests\TestCase;
@@ -8,6 +8,15 @@ use App\Models\User;
 
 class DeleteTest extends TestCase
 {
+    protected User $user;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->user = User::factory()->create();
+    }
+
+
     public function test_require_auth()
     {
         $comment = Comment::factory()->create();
@@ -17,29 +26,38 @@ class DeleteTest extends TestCase
 
     public function test_user_can_delete_comment()
     {
-        $user = User::factory()->create();
-        $comment = Comment::factory()->for($user)->create();
-        $response = $this->actingAs($user)->delete(route('comments.destroy', $comment));
+        $comment = Comment::factory()->for($this->user)->create();
+        $response = $this->actingAs($this->user)->delete(route('comments.destroy', $comment));
         $this->assertModelMissing($comment);
         $response->assertRedirect(route('posts.show', $comment->post));
     }
 
     public function test_prevent_users_from_deleting_others_comments()
     {
-        $user = User::factory()->create();
         $comment = Comment::factory()->create();
-        $response = $this->actingAs($user)->delete(route('comments.destroy', $comment));
+        $response = $this->actingAs($this->user)->delete(route('comments.destroy', $comment));
         $response->assertForbidden();
     }
 
     public function test_prevents_deleting_comments_after_an_hour()
     {   
         $this->freezeTime();
-        $user = User::factory()->create();
-        $comment = Comment::factory()->for($user)->create();
-
+        $comment = Comment::factory()->for($this->user)->create();
         $this->travel(1)->hour();
-        $response = $this->actingAs($user)->delete(route('comments.destroy', $comment));
+        $response = $this->actingAs($this->user)->delete(route('comments.destroy', $comment));
         $response->assertForbidden();
     }
+
+    public function test_redirects_to_post_with_the_pagination()
+    {
+        $comment = Comment::factory()->for($this->user)->create();
+        $response = $this->actingAs($this->user)->delete(
+            route('comments.destroy', ['comment' => $comment, 'page' => 2]),
+        );
+        $response->assertRedirect(
+            route('posts.show', ['post' => $comment->post, 'page' => 2])
+        );
+    }
 }
+
+
