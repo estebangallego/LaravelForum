2ba946d090e8230f64d8453b6aec8e440420ba76
Esteban Gallego <egallego00@gmail.com>
2024-09-26 10:33:10 -0400

    Initialize editComments test

---
+++ tests/Feature/Controllers/EditComments.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Tests\Feature\Controllers;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class EditComments extends TestCase
+{
+    // Edit comments requires authentication
+
+    // Can update comments
+
+
+    // Redirects to post show page
+
+
+    // Redirects to post show page with pagination
+
+
+    // Can not update comments that are not yours
+
+    // Can delete comments
+}
