02bd6ce5aac09d7aae94fc5720d45e47af646a3a
Esteban Gallego <egallego00@gmail.com>
2024-11-08 15:38:31 -0500

    Post article

---
+++ resources/js/Pages/Posts/Create.vue
@@ -0,0 +1,51 @@
+<template>
+    <AppLayout title="Create a Post">
+        <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                 Create a Post 
+            </h2>
+        </template>
+
+        <Container>
+
+            <article>
+                <form @submit.prevent="createPost">
+                <div>
+                    <InputLabel for="title"> Title </InputLabel>
+                    <TextInput id="title" v-model="form.title" class="mt-2 w-full" aria-placeholder="Give it a great title" />
+                    <InputError :message="form.errors.title" class="mt-2" />
+                </div>
+                <div>
+                    <InputLabel for="body" class="mt-4"> Body </InputLabel>
+                    <textarea id="body" v-model="form.body" type="text" class="mt-2 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" rows="12" />
+                    <InputError :message="form.errors.body" class="mt-2" />
+                </div>
+                <div>
+                    <PrimaryButton type="submit" class="mt-4">Create Post</PrimaryButton>
+                </div>
+            </form>
+            </article>
+    
+        </Container>
+
+    </AppLayout>
+</template>
+
+<script setup>
+    import AppLayout from '@/Layouts/AppLayout.vue';
+    import Container from '@/Components/Container.vue';
+    import PrimaryButton from '@/Components/PrimaryButton.vue';
+    import InputLabel from '@/Components/InputLabel.vue';
+    import TextInput from '@/Components/TextInput.vue';
+    import InputError from '@/Components/InputError.vue';
+    import { useForm } from '@inertiajs/vue3';
+
+    const form = useForm({
+        body: '',
+        title: ''
+    });
+
+    const createPost = () => form.post(route('posts.store'));
+    
+
+</script>
--- tests/Feature/Controllers/PostController/CreateTest.php
+++ tests/Feature/Controllers/PostController/CreateTest.php
@@ -3,12 +3,33 @@
 namespace Tests\Feature\Controllers\PostController;
 
 use Tests\TestCase;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class CreateTest extends TestCase
 {
 
+    use RefreshDatabase;
+
+    protected User $user;
+    protected array $validData;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->user = User::factory()->create();
+    }
+
     public function test_it_requires_authentication()
     {
         $this->get(route('posts.create'))->assertRedirect(route('login')); 
+    }
+
+    public function test_component() {
+
+        $this->actingAs($this->user)
+            ->get(route('posts.create'))
+            ->assertComponent('Posts/Create');
+
     }
 }
