7957b5229ef1e505156dc977d90d9b8c5af0194f
Esteban Gallego <egallego00@gmail.com>
2025-06-02 10:43:35 -0400

    Fix redirect bug

--- tests/Feature/Controllers/PostController/ShowTest.php
+++ tests/Feature/Controllers/PostController/ShowTest.php
@@ -7,6 +7,7 @@ use App\Http\Resources\PostResource;
 use App\Models\Comment;
 use App\Models\Post;
 use Tests\TestCase;
+use Illuminate\Support\Str;
 
 class ShowTest extends TestCase
 {
@@ -32,5 +33,15 @@ class ShowTest extends TestCase
 
         $response = $this->get($post->showRoute());
         $response->assertPaginatedResource('comments', CommentResource::collection($comments->reverse()));
+    }
+
+    public function test_redirects_if_the_slug_is_incorrect(): void
+    {
+        $post = Post::factory()->create(['title' => 'Hello World']);
+        // Use incorrect slug
+        $response = $this->get(route('posts.show', ['post' => $post, 'slug' => 'hello']));
+        // Expect redirect to correct slug
+        $response->assertRedirect(route('posts.show', ['post' => $post, 'slug' => 'hello-world']));
+        $response->assertStatus(301);
     }
 }
